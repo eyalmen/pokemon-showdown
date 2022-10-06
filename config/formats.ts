b3838b1b75ddd99021f9dd319b68701e37e3f4b5
Oracle Public Cloud User <opc@pseudos-competitive.subnet05271522.vcn05271522.oraclevcn.com>
2022-10-06 17:58:01 +0000

    zamn

--- config/formats.ts
+++ config/formats.ts
@@ -17,6 +17,8 @@ New sections will be added to the bottom of the specified column.
 The column value will be ignored for repeat sections.
 */
 
+import { TIERS } from '../tools/set-import/importer';
+
 export const Formats: FormatList = [
 
 	// Sw/Sh Singles
@@ -49,6 +51,35 @@ export const Formats: FormatList = [
 			ruleset: ['Standard NatDex', 'Dynamax Clause', 'Evasion Items Clause', 'Z-Move Clause'],
 			banlist: ['AG', 'Shadow Tag', 'Baton Pass', 'Uber', 'Suspect', 'OU', 'UU', "RU", "NU", "PU", "ZU", "Huge Power"]
 		},
+		{
+		name: "[Gen 8] UU Deluxe",
+		threads: [
+				//`&bullet; <a href="https://www.smogon.com/forums/threads/3666135/">National Dex Metagame Discussion</a>`,
+				//`&bullet; <a href="https://www.smogon.com/forums/threads/3667921/">National Dex Sample Teams</a>`,
+				//`&bullet; <a href="https://www.smogon.com/forums/threads/3666572/">National Dex Viability Rankings</a>`,
+		],
+
+		mod: 'gen8deluxe',
+		ruleset: ['Standard NatDex', 'Z-Move Clause','OHKO Clause', 'Evasion Moves Clause', 'Evasion Items Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod'],
+		banlist: [
+			"Uber", "AG", "OU"/*, "OUD"*/, "Teleport", "Quick Claw", "Acupressure", "Shadow Tag", "Baton Pass", "King's Rock",
+		],
+	},
+	{
+		name: "[Gen 8] OU Deluxe",
+		threads: [
+				//`&bullet; <a href="https://www.smogon.com/forums/threads/3666135/">National Dex Metagame Discussion</a>`,
+				//`&bullet; <a href="https://www.smogon.com/forums/threads/3667921/">National Dex Sample Teams</a>`,
+				//`&bullet; <a href="https://www.smogon.com/forums/threads/3666572/">National Dex Viability Rankings</a>`,
+		],
+
+		mod: 'gen8deluxe',
+		ruleset: ['Standard NatDex', 'Z-Move Clause','OHKO Clause', 'Evasion Moves Clause', 'Evasion Items Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod'],
+		banlist: [
+			'Uber', 'AG', "Pseudo", 'Arena Trap', 'Moody', 'Power Construct', 'Shadow Tag', 'Baton Pass', "King's Rock",
+		],
+	},
+
 //	{
   //              name: "[Gen 8] Pseudos Suspect",
     //            threads: [
@@ -498,26 +529,8 @@ export const Formats: FormatList = [
 			'Arena Trap', 'Moody', 'Power Construct', 'Shadow Tag', 'Baton Pass',
 		],
 	},
-	{
-		name: "[Gen 8] OU Deluxe",
-		threads: [
-				//`&bullet; <a href="https://www.smogon.com/forums/threads/3666135/">National Dex Metagame Discussion</a>`,
-				//`&bullet; <a href="https://www.smogon.com/forums/threads/3667921/">National Dex Sample Teams</a>`,
-				//`&bullet; <a href="https://www.smogon.com/forums/threads/3666572/">National Dex Viability Rankings</a>`,
-		],
-
-		mod: 'gen8',
-		ruleset: ['Standard NatDex', 'Z-Move Clause','OHKO Clause', 'Evasion Moves Clause', 'Evasion Items Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod'],
-		banlist: [
-			'Alakazam-Mega', 'Arceus', 'Blastoise-Mega', 'Blaziken', 'Calyrex-Ice', 'Calyrex-Shadow','Darmanitan-Galar',
-			'Deoxys-Attack', 'Deoxys-Base', 'Deoxys-Speed', 'Dracovish', 'Eternatus','Gengar-Mega',
-			'Giratina-Origin', 'Groudon', 'Ho-Oh', 'Kangaskhan-Mega', 'Kyogre','Kyurem-White', 'Landorus-Base', 'Lucario-Mega',
-			'Lunala','Marshadow','Mewtwo', 'Necrozma-Dawn-Wings', 'Necrozma-Dusk-Mane',
-			'Necrozma-Ultra', 'Palkia', 'Rayquaza','Salamence-Mega', 'Shaymin-Sky',
-			'Urshifu-Base', 'Xerneas', 'Yveltal', 'Zacian', 'Zacian-Crowned', 'Zamazenta','Zekrom', 'Zygarde-Base', 'Zygarde-Complete',
-			'Arena Trap', 'Moody', 'Power Construct', 'Shadow Tag', 'Baton Pass', "King's Rock",
-		],
-	},
+	
+	
 	{
 		name: "[Gen 8] National Dex UU",
 		threads: [
