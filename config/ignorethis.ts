a7bfa00483c8b87a98a9284e32dcd668dfdbcbc1
Oracle Public Cloud User <opc@pseudos-competitive.subnet05271522.vcn05271522.oraclevcn.com>
2022-10-07 12:59:24 +0000

    ue

---
+++ config/ignorethis.ts
@@ -0,0 +1,17 @@
+export const Formats: FormatList = [
+    {
+		section: "Sw/Sh Singles",
+    },
+    {
+		name: "[Gen 8] Pseudos",
+		threads: [
+		//	`&bullet; <a href="https://www.smogon.com/forums/threads/3676539/">Ubers Metagame Discussion</a>`,
+		//	`&bullet; <a href="https://www.smogon.com/forums/threads/3675564/">Ubers Sample Teams</a>`,
+		//	`&bullet; <a href="https://www.smogon.com/forums/threads/3675194/">Ubers Viability Rankings</a>`,
+		],
+
+		mod: 'gen8',
+		ruleset: ['Standard', 'Dynamax Clause'],
+                banlist: ['AG', 'Shadow Tag', 'Baton Pass' ,'Calyrex-Ice','Calyrex-Shadow','Darmanitan-Galar','Darmanitan-Galar-Zen','Dracovish','Eternatus','Giratina','Giratina-Origin','Groudon','Ho-Oh','Kyogre','Landorus','Lunala','Marshadow','Mewtwo','Necrozma-Dusk Mane','Rayquaza','Urshifu','Xerneas','Yveltal','Zamazenta-Crowned','Zekrom','Zygarde','Zygarde-Complete'], 
+	},
+];
